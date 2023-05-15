d77fed4aa012e59d73595e17d1650b7422e4e754
fastwalkergy <106460370+fastwalkergy@users.noreply.github.com>
2023-05-15 20:14:57 +0800

    c

--- efiles/SEO-PC.js
+++ efiles/SEO-PC.js
@@ -1,15 +1,11 @@
 // 创建 meta 元素
 var meta = document.createElement('meta');
 meta.setAttribute('name', 'keywords');
-meta.setAttribute('content', '关键词1, 关键词2, 关键词3');
+meta.setAttribute('content', 'PS工作室, PS, PSK, Phantom-Shadow, Phantom-Shadow-Kit, win工具箱, ');
 
-// 创建 title 元素
-var title = document.createElement('title');
-title.innerHTML = '您网页标题';
 
 // 获取 head 元素
 var head = document.getElementsByTagName('head')[0];
 
 // 将 meta 和 title 元素添加到头部
 head.appendChild(meta);
-head.appendChild(title);
